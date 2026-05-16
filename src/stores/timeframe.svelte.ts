72a351d97451eec6f4b95f3a4cecd2003af01b7c
Aldi <34829303+aldinh777@users.noreply.github.com>
2026-05-16 13:19:34 +0800

    Update timeframe.svelte.ts

--- src/stores/timeframe.svelte.ts
+++ src/stores/timeframe.svelte.ts
@@ -17,7 +17,9 @@ class TimeFrameStore {
     { label: "7D", hours: 7 * 24, interval: "1h" },
     { label: "1M", hours: 30 * 24, interval: "4h" },
     { label: "3M", hours: 90 * 24, interval: "12h" },
-    { label: "1Y", hours: 360 * 24, interval: "1d" },
+    { label: "6M", hours: 180 * 24, interval: "1d" },
+    { label: "1Y", hours: 360 * 24, interval: "3d" },
+    { label: "4Y", hours: 4 * 360 * 24, interval: "1w" },
   ];
   fxTimeframes: FxTimeframe[] = [
     { label: "7D", days: 7 },
